b27b61060ac41b6b5ecd0bd7693bfba23a4a7a53
Laura Martinez <lauramvaldi@gmail.com>
2022-04-04 16:21:36 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeroesModule } from './Heroes/heroes.module';
+import { ContadorModule } from './Contador/Contador/contador.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+ 
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
